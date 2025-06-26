c72bb2750637ae8aa20876c5019acee581eae0ad
FaridEspinoza <farid23jos@outlook.com>
2025-06-26 13:28:30 -0600

    Clase List
    Se crea la interfaz list con los metodos necesarios

---
+++ src/Lists/List.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Farid
+ */
+public interface List <T> {
+    public boolean add(T t);
+    
+    public void showAll();
+    
+}
